5ff78ba1cf23d95701642dda9b7ed0082ea0ba03
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-11-22 20:50:34 +0100

    background gin bottles okay

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function loadEvent() {
+  const rootElement = document.getElementById("root");
+  rootElement.insertAdjacentHTML("beforeend", `
+  <div class="bence">
+    <div class="bottle-container">
+      <img class=" bottle" src="gin-bottles/gin-1.jpg" alt="gin1" >
+      <img class=" bottle" src="gin-bottles/gin-2.jpg" alt="gin2" >
+      <img class=" bottle" src="gin-bottles/gin-3.jpg" alt="gin3" >
+      <img class=" bottle bottle4" src="gin-bottles/gin-4.jpg" alt="gin4" >
+    </div>
+    <div class="text-container">
+      <div class="logo"><img src="gin-bottles/logo.jpg" alt="logo" ></div>
+      <div class="animated-text"><span class="text1">Citadelle Gin,</span><br><span class="text2">LE gin de france</span></div>
+      <div class="question-text">Avez-vous l’âge légal afin de consommer de l’alcool dans votre pays ?</div>
+      <div class="buttons">GOMBOK...................................HELYE</div>
+    </div>
+  </div>
+
+  </div>`);
+
+					
+
+  
    
 }
 
