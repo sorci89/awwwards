ee378b78d8f2db5ca2011ed262ed8707c0b3ed80
DooZeeR <zsolt.ordas@gmail.com>
2021-11-23 03:47:06 +0100

    Stripes Running Across The Screen Responsive

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+
+// Zsolt --- Átszaladó csíkok-----------------
+function stripesRunningAcrossTheScreen (){
+    const stripeBox = document.createElement("div");
+    stripeBox.setAttribute("id", "stripebox");
+    let yellow = Math.floor(Math.random() * 8) + 1;
+    for (let i = 1; i < 9; i++) {
+        if (i === yellow) {
+            const stripe = document.createElement("div");
+            stripe.className = `yellow poz${i}`;
+            stripeBox.insertAdjacentElement('beforeend', stripe);
+        } else {
+            const stripe = document.createElement("div");
+            stripe.className = `basicblue poz${i}`; 
+            stripeBox.insertAdjacentElement('beforeend', stripe);
+        }
+    }
+    document.getElementById('root').insertAdjacentElement('beforeend', stripeBox);
+}
+
+
+
+
+
 function loadEvent() {
-   
+    stripesRunningAcrossTheScreen ();
 }
 
 window.addEventListener("load", loadEvent);
